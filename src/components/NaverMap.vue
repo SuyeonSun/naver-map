ca40cd8086109ec546051fe6c4ca17a20a1583e9
Suyeon Sun <sysun@autocrypt.io>
2023-02-01 14:40:45 +0900

    naver api를 사용하여 지도 띄우기

---
+++ src/components/NaverMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="naverMap" style="width:100%;height:400px;"></div>
+</template>
+
+<script setup>
+import {onMounted} from 'vue';
+
+onMounted(() => {
+  var map = null;
+
+  initMap();
+
+  function initMap() {
+    map = new naver.maps.Map('naverMap', {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10,
+    });
+  }
+});
+</script>
+
+<style>
+#naverMap {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NaverMap from 'components/NaverMap.vue';
+</script>
+
+<template>
+  <q-page class="row items-center justify-evenly">
+    <naver-map></naver-map>
+  </q-page>
+</template>
